411cc278a140927e84c8b4013813e5529dd552be
martirosyangrig <grigor.martriosyan2000@mail.ru>
2024-03-19 17:54:03 +0400

    Translatable Texts, i18n , ResourceModel

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -3,8 +3,9 @@ sap.ui.define(
         "sap/ui/core/mvc/Controller",
         "sap/m/MessageToast",
         "sap/ui/model/json/JSONModel",
+        "sap/ui/model/resource/ResourceModel",
     ],
-    function (Controller, MessageToast, JSONModel) {
+    function (Controller, MessageToast, JSONModel, ResourceModel) {
         "use strict";
 
         return Controller.extend("sap.ui.demo.walkthrough.App", {
@@ -19,11 +20,29 @@ sap.ui.define(
 
                 const oModel = new JSONModel(oData);
                 this.getView().setModel(oModel);
+
+                // set i18n model on view
+                const i18nModel = new ResourceModel({
+                    bundleName: "sap.ui.demo.walkthrough.i18n.i18n",
+                    supportedLocales: [""],
+                    fallbackLocale: "",
+                });
+
+                this.getView().setModel(i18nModel, "i18n");
             },
 
             onShowHello: function () {
+                // read msg from i18n model
+                const oBundle = this.getView()
+                    .getModel("i18n")
+                    .getResourceBundle();
+                const sRecipient = this.getView()
+                    .getModel()
+                    .getProperty("/recipient/name");
+                const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
                 // Show a native or vanilla JS alert
-                MessageToast.show("Hello guys from module");
+                MessageToast.show(sMsg);
             },
         });
     }
