5e4a9be06c944264cde869ea995b4e4345ef41c9
martirosyangrig <grigor.martriosyan2000@mail.ru>
2024-03-20 16:46:14 +0400

    Nested Views

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,25 +1,11 @@
 sap.ui.define(
     [
         "sap/ui/core/mvc/Controller",
-        "sap/m/MessageToast",
     ],
-    function (Controller, MessageToast) {
+    function (Controller) {
         "use strict";
 
         return Controller.extend("sap.ui.demo.walkthrough.App", {
-            onShowHello: function () {
-                // read msg from i18n model
-                const oBundle = this.getView()
-                    .getModel("i18n")
-                    .getResourceBundle();
-                const sRecipient = this.getView()
-                    .getModel()
-                    .getProperty("/recipient/name");
-                const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-                // Show a native or vanilla JS alert
-                MessageToast.show(sMsg);
-            },
         });
     }
 );
---
+++ webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,25 @@
+sap.ui.define(
+    ["sap/ui/core/mvc/Controller", "sap/m/MessageToast"],
+    function (Controller, MessageToast) {
+        "use strict";
+
+        return Controller.extend(
+            "sap.ui.demp.walkthrough.controller.HelloPanel",
+            {
+                onShowHello: function () {
+                    // read msg from i18n model
+                    const oBundle = this.getView()
+                        .getModel("i18n")
+                        .getResourceBundle();
+                    const sRecipient = this.getView()
+                        .getModel()
+                        .getProperty("/recipient/name");
+                    const sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+                    // Show a native or vanilla JS alert
+                    MessageToast.show(sMsg);
+                },
+            }
+        );
+    }
+);
