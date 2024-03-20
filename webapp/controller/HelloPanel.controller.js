a36bcc5cb63d1b5588fbeda9e91cbd6bcbccde9f
martirosyangrig <grigor.martriosyan2000@mail.ru>
2024-03-20 18:34:41 +0400

    Dialogs and Fragments

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,44 +1,40 @@
 sap.ui.define(
     [
-        "sap/ui/core/mvc/Controller", 
+        "sap/ui/core/mvc/Controller",
         "sap/m/MessageToast",
-        "sap/ui/core/Fragment"
+        "sap/ui/core/Fragment",
     ],
-    function (Controller, MessageToast, Fragment) {
+    function (Controller, MessageToast, Fragement) {
         "use strict";
-
         return Controller.extend(
-            "sap.ui.demp.walkthrough.controller.HelloPanel",
+            "sap.ui.demo.walkthrough.controller.HelloPanel",
             {
                 onShowHello: function () {
                     // read msg from i18n model
-                    const oBundle = this.getView()
+                    var oBundle = this.getView()
                         .getModel("i18n")
                         .getResourceBundle();
-                    const sRecipient = this.getView()
+                    var sRecipient = this.getView()
                         .getModel()
                         .getProperty("/recipient/name");
-                    const sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
-                    // Show a native or vanilla JS alert
+                    var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+                    // Show message
                     MessageToast.show(sMsg);
                 },
-
                 onOpenDialog: function () {
-                    const oView = this.getView();
+                    var oView = this.getView();
 
-                    //create the dialog lazily
+                    // create the dialog lazily
                     if (!this.byId("helloDialog")) {
-                        // load async XML fragment
-                        Fragment.load({
+                        // load asynchronous XML fragment
+                        Fragement.load({
                             id: oView.getId(),
                             name: "sap.ui.demo.walkthrough.view.HelloDialog",
-                            controller: this
                         }).then(function (oDialog) {
-                            //  connect dialog to the root view of this component (models, lifecycle)
+                            // connect dialog to the root view of this component (models, lifecycle)
                             oView.addDependent(oDialog);
-                            oDialog.open()
-                        })
+                            oDialog.open();
+                        });
                     } else {
                         this.byId("helloDialog").open();
                     }
